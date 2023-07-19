5665467ef4ee9f7ed0ff7fbb81957db99e5787d3
Jaewoong Kim <rorhcdream@gmail.com>
2023-07-19 09:53:32 +0900

    Update setting script

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -1,69 +1,81 @@
 #!/bin/bash
+set -ex
 
 basedir=$(dirname $0)
+sudo=${SUDO:+sudo}
+
+# install packages
+export DEBIAN_FRONTEND=noninteractive
+packages=("git" "zsh" "curl" "tmux" "vim" "ripgrep" "wget")
+if [ -x "$(command -v apk)" ];       then $sudo apk add --no-cache ${packages[@]} zsh-vcs grep
+elif [ -x "$(command -v apt-get)" ]; then $sudo apt update && apt install -y ${packages[@]} language-pack-en
+elif [ -x "$(command -v brew)" ];     then $sudo brew install ${packages[@]}
+else echo "FAILED TO INSTALL PACKAGE: Package manager not found."; fi
 
 # machine configuration
 uname_out="$(uname -s)"
 case "${uname_out}" in
 	Linux*)		machine=Linux;;
-	Darwin*)    	machine=Mac;;
-    	*)          	machine="UNKNOWN:${uname_out}"
+	Darwin*)    machine=Mac;;
+    *)          machine="UNKNOWN:${uname_out}"
 esac
+os_id=$(grep -ioP '^ID=\K.+' /etc/os-release)
 echo "Machine: $machine"
-
-# install packages
-packages=("git" "zsh" "curl" "tmux" "vim" "ripgrep")
-if [ -x "$(command -v apk)" ];       then apk add --no-cache ${packages[@]} zsh-vcs
-elif [ -x "$(command -v apt-get)" ]; then apt update && apt install -y ${packages[@]} language-pack-en
-elif [ -x "$(command -v brew)" ];     then brew install ${packages[@]}
-else echo "FAILED TO INSTALL PACKAGE: Package manager not found."; fi
+echo "OS: $os_id"
 
 # install oh-my-zsh and its extensions
 echo "Installing oh-my-zsh and its extensions"
-yes | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ||
-{ echo "Failed"; exit 1; }
+yes | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # install tmux plugin manager
 echo "Installing tmux plugin manager"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm ||
-{ echo "Failed"; exit 1; }
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# install vim plugin manager
-echo "Installing vim plugin manager"
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim ||
-{ echo "Failed"; exit 1; }
+# # install vim plugin manager
+# echo "Installing vim plugin manager"
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim ||
+# { echo "Failed"; exit 1; }
 
-# instal pyenv
+# install pyenv
 curl https://pyenv.run | bash
+
+# install nvm and node
+if [ $os_id = "alpine" ]; then
+    $sudo apk add nodejs
+else
+    touch ~/.bashrc
+    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+    source ~/.bashrc
+    nvm install node
+fi
 
 # install nvim
 echo "Installing nvim"
 if [ $machine = "Mac" ]; then
-	brew install neovim ||
-	{ echo "Failed"; exit 1; }
-else 
-	wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage && 
-	chmod +x nvim.appimage && 
-	sudo mv nvim.appimage /usr/local/bin/nvim ||
-	{ echo "Failed"; exit 1; }
+	$sudo brew install neovim
+elif [ $os_id = "alpine" ]; then
+    $sudo apk add --no-cache neovim
+else
+    wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+    tar -xzvf nvim-linux64.tar.gz
+    (test -e /nvim-linux64 || $sudo -mv nvim-linux64 /; true)
+    $sudo ln -s /nvim-linux64/bin/nvim /usr/local/bin/nvim
 fi
 
 # install nvim plugin manager
 echo "Installing nvim plugin manager"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' ||
-{ echo "Failed"; exit 1; }
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # make symbolic links of dotfiles
-$basedir/make_symlinks.sh
+SUDO=$sudo $basedir/make_symlinks.sh
 
-# install vim plugins
-echo "Installing vim plugins"
-vim +PlugInstall +qall ||
-{ echo "Failed"; exit 1; }
+# install nvim plugins
+echo "Installing nvim plugins"
+nvim -e -s -u ~/.vimrc +PlugInstall || true
 
 
 
