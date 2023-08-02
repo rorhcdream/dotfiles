c04afa9ac01486c8eb5eb042e626eadb9bffd79e
Jaewoong Kim <rorhcdream@gmail.com>
2023-08-02 22:19:08 +0900

    Remove unused files

--- scripts/install.sh
+++
@@ -1,4 +0,0 @@
-if [ -x "$(command -v apk)" ];       then apk add --no-cache $@
-elif [ -x "$(command -v apt-get)" ]; then apt install -y $@
-elif [ -x "$(command -v brew)" ];     then brew install $@
-else echo "FAILED TO INSTALL PACKAGE: Package manager not found."; fi
