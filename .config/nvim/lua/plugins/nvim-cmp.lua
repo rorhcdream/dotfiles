df6564b71987f0162ba4e76fd8835d0d0a6fc28d
Jaewoong Kim <rorhcdream@gmail.com>
2024-05-08 00:47:20 +0000

    Ignore whitespace on autocompletion

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -18,9 +18,35 @@ return {
 
         require('luasnip.loaders.from_vscode').lazy_load()
 
+        local is_whitespace = function()
+            -- returns true if the character under the cursor is whitespace.
+            local col = vim.fn.col('.') - 1
+            local line = vim.fn.getline('.')
+            local char_under_cursor = string.sub(line, col, col)
+
+            if col == 0 or string.match(char_under_cursor, '%s') then
+                return true
+            else
+                return false
+            end
+        end
+         
         cmp.setup({
+            enabled = function()
+                return not is_whitespace()
+            end,
             completion = {
-                completeopt = 'menu,menuone,preview',
+                completeopt = 'menu,menuone',
+                get_trigger_characters = function(chars)
+                  print(table.concat(chars, ", "))
+                  local new_chars = {}
+                  for _, char in ipairs(chars) do
+                    if char ~= ' ' then
+                      table.insert(new_chars, char)
+                    end
+                  end
+                  return new_chars
+                end
             },
             snippet = {
                 expand = function(args)
@@ -33,10 +59,10 @@ return {
                 ['<Tab>'] = cmp.mapping.confirm({ select = false }),
             }),
             sources = cmp.config.sources({
+                { name = 'nvim-lsp' },
                 { name = 'luasnip' },
                 { name = 'buffer' },
                 { name = 'path' },
-                { name = 'nvim-lsp' },
                 { name = 'cmdline' },
             }),
         })
