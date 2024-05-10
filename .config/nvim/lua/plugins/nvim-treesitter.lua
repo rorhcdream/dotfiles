f09c1861b95c7b3f84108f876c072c7abe0382d5
Jaewoong Kim <rorhcdream@gmail.com>
2024-05-10 00:49:39 +0000

    Install and setup treesitter

---
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,31 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+        local configs = require("nvim-treesitter.configs")
+        local disableFunc = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end
+
+        configs.setup({
+            ensure_installed = { 
+                "c", "lua", "vim", "vimdoc", "query", 
+                "javascript", "typescript", "html",
+                "go", "python", "rust",
+            },
+            sync_install = false,
+            highlight = { 
+                enable = true,
+                disable = disableFunc,
+            },
+            indent = { 
+                enable = true,
+                disable = disableFunc,
+            },  
+        })
+    end
+}
