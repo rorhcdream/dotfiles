a7f92d598623311540c2fd6547e0cfd976a5b618
Jaewoong Kim <rorhcdream@gmail.com>
2024-05-11 11:41:13 +0900

    Add rust-analyzer lsp server

--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -59,5 +59,10 @@ return {
 			capabilities = capabilities,
 			on_attach = on_attach,
 		})
+
+		lspconfig["rust_analyzer"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 	end,
 }
