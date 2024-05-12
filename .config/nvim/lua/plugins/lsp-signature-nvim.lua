3d37d763b93578496e1f79c1e684e5c21ceb286c
Jaewoong Kim <rorhcdream@gmail.com>
2024-05-12 20:26:23 +0900

    No lazy load for lsp_signature.nvim

--- .config/nvim/lua/plugins/lsp-signature-nvim.lua
+++ .config/nvim/lua/plugins/lsp-signature-nvim.lua
@@ -1,6 +1,6 @@
 return {
 	"ray-x/lsp_signature.nvim",
-	event = "VeryLazy",
+	-- event = "VeryLazy", -- Does not work in some cases with lazy loading
 	config = function()
 		require("lsp_signature").setup({
 			hint_prefix = "",
