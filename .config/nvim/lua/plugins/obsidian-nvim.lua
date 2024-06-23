7a01727ea816afd06a6f8912cd552f75b2a50ad8
Jaewoong Kim <rorhcdream@gmail.com>
2024-06-23 16:23:41 +0900

    Update obsidian config for note path and daily notes

--- .config/nvim/lua/plugins/obsidian-nvim.lua
+++ .config/nvim/lua/plugins/obsidian-nvim.lua
@@ -23,5 +23,19 @@ return {
 				["-"] = { char = "x", hl_group = "ObsidianDone" },
 			},
 		},
+		daily_notes = {
+			folder = "Daily notes",
+		},
+		---@param spec { id: string, dir: obsidian.Path, title: string|? }
+		---@return string|obsidian.Path The full path to the new note.
+		note_path_func = function(spec)
+			if string.len(spec.title) == 0 then
+				local path = spec.dir / tostring(spec.id)
+				return path:with_suffix(".md")
+			end
+
+			local path = spec.dir / spec.title
+			return path:with_suffix(".md")
+		end,
 	},
 }
