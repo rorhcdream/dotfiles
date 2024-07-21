3b17008d943c8e98d5f571d9f974aa320de65225
Jaewoong Kim <rorhcdream@gmail.com>
2024-07-21 17:44:56 +0900

    Set efmls for python formatting

--- .config/nvim/lua/plugins/lsp/efmls.lua
+++ .config/nvim/lua/plugins/lsp/efmls.lua
@@ -1,18 +1,16 @@
 return {
 	"creativenull/efmls-configs-nvim",
-	enabled = false, -- language servers already have built-in formatters
 	version = "v1.x.x", -- version is optional, but recommended
 	dependencies = {
 		"neovim/nvim-lspconfig",
-		"williamboman/mason.nvim",
 	},
 	-- https://github.com/creativenull/efmls-configs-nvim?tab=readme-ov-file#setup
 	config = function()
 		local languages = {
-			---- lua-ls already has a built-in formatter
-			-- lua = {
-			-- 	require("efmls-configs.formatters.stylua"),
-			-- },
+			python = {
+				require('efmls-configs.formatters.black'),
+				require('efmls-configs.formatters.isort'),
+			},
 		}
 
 		local efmls_config = {
